ae61d0c59defc748142561cbe36f122a19e6a61d
svsakharovskiy <svsakharovskiy@alfabank.ru>
2022-04-17 23:08:33 +0200

    Entry point of widget

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { hashCode } from './utils/hash-code';
+import { Poll } from './poll';
+import './styles.css';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const widgetContainers = document.querySelectorAll('.opinary-poll-widget');
+const widgetConfigList = []; // contains all loaded widget configs in the app / page
+
+widgetContainers.forEach(container => {
+    const widget = createRoot(container);
+    const config = container.getAttribute('data-config');
+
+    if (config) {
+        const pollId = hashCode(config);
+        const pathname = window && window.localStorage;
+
+        /**
+         * Check existing poll with same config in the same page
+         */
+        const isWidgetLoad = widgetConfigList.find(
+            widgetConfig => widgetConfig.pollId === pollId && widgetConfig.pathname === pathname
+        );
+
+        /**
+         * If we don't have the same widget configs in this very page, we create new widget and add to page
+         * If we have same widget configs in the same page, we skip iteratoin.
+         */
+        if (!isWidgetLoad) {
+            // Add poll id into the list to check doublicates in the future
+            widgetConfigList.push({ pollId, pathname });
+
+            widget.render(
+                <React.StrictMode>
+                    <Poll config={config} pollId={pollId} />
+                </React.StrictMode>
+            );
+        }
+    }
+});

d5675ee22469658351040c6c1380c3fc94e0db6f
svsakharovskiy <svsakharovskiy@alfabank.ru>
2022-04-17 23:00:46 +0200

    Hook to init and provides method to work with poll

---
+++ src/poll/hooks/use-get-poll-data.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react';
+
+/**
+ *
+ * Hook needs to load initial config and data from localStorage.
+ * The hook provides methods and properties to work with a poll.
+ */
+export const useGetPollData = ({ config, pollId }) => {
+    const [question, setQuestion] = useState('');
+    const [options, setOptions] = useState([]);
+    const [error, setError] = useState('');
+    const [isVoted, setIsVoted] = useState(false);
+
+    useEffect(() => {
+        try {
+            if (!config) {
+                throw new Error('Config is empty!');
+            }
+
+            const parserdConfig = JSON.parse(config);
+
+            if (!parserdConfig) {
+                throw new Error('Config is empty!');
+            }
+
+            if (!parserdConfig.question) {
+                throw new Error('You did not set a question!');
+            }
+
+            if (!parserdConfig.options || parserdConfig.options.length < 1) {
+                throw new Error('You did not set options!');
+            }
+
+            const pollResult = JSON.parse(localStorage.getItem(pollId));
+
+            const preparedOptions = parserdConfig.options.map((option, index) => ({
+                id: index,
+                optionText: option,
+                votes: (pollResult && pollResult[index]) || 0,
+            }));
+
+            setQuestion(parserdConfig.question);
+            setOptions(preparedOptions);
+        } catch (error) {
+            setError(error);
+        }
+    }, [config, pollId]);
+
+    const updateVote = optionId => {
+        const option = options.find(({ id }) => id === optionId);
+        option.votes++;
+        options[optionId] = option;
+
+        const pollResult = options.map(({ votes }) => votes);
+        localStorage.setItem(pollId, JSON.stringify(pollResult));
+
+        setOptions(options);
+        setIsVoted(true);
+    };
+
+    return {
+        error,
+        question,
+        options,
+        updateVote,
+        isVoted,
+    };
+};

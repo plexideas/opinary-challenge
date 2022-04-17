4140912fbdeb7ae50fcc6608f1e57d044b90be16
svsakharovskiy <svsakharovskiy@alfabank.ru>
2022-04-17 23:53:33 +0200

    Configs files

---
+++ src/configs.js
@@ -0,0 +1,2 @@
+export const WIDGET_CLASSNAME = '.opinary-poll-widget';
+export const CONFIG_DATA_ATTRIBUTE = 'data-config';
---
+++ src/poll/configs.js
@@ -0,0 +1,7 @@
+export const CONFIG_NOT_DEFINED = 'Config is empty!';
+
+export const QUESTION_NOT_DEFINED = 'You did not set a question!';
+
+export const OPTIONS_NOT_DEFINED = 'You did not set options!';
+
+export const ERROR_TITLE = 'Ups... somethig went wrong 🫣';
